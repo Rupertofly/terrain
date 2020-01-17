241ab4598bba9166179ee9175f03aac31e793a51
Ruby Quail <ruby@rubyquail.design>
2020-01-17 17:34:25 +1100

    getting to work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './terrain';
+import { Point } from './terrain';
+
+console.log('hey');
+console.log('you');
+let x = new Point(1, 2, 3);
+let y = Point.new(x);
+let z = Point.new(3, 2);
+console.log(x, y, z);
